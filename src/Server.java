3084f6fcca20945618d81534183d4e4a69012df9
Veershah26 <shah.veer18@gmail.com>
2021-11-20 16:12:16 +0530

    Updated server.java with basic Server connection socket module

--- src/Server.java
+++ src/Server.java
@@ -9,3 +9,22 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.lang.Integer;
 import java.lang.Runnable;
+
+@SuppressWarnings("unused")
+
+public class Server {
+    public static ArrayList<Fileinfo> global_array = new ArrayList<FileInfo>();
+
+    @SuppressWarnings("resource")
+
+    public Server() throws NumberFormatException, IOException {
+        ServerSocket server_socket = null;
+        Socket socket = null;
+        try {
+            server_socket = new ServerSocket(7799);
+            System.out.println("Server Initiated");
+            System.out.println(" ");
+            System.out.println("Waiting For Client Connection ...");
+        }
+    }
+}
