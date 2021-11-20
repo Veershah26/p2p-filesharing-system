900d43fd5040bfaccf22c2ef9ed8f0e653fe254f
Veershah26 <shah.veer18@gmail.com>
2021-11-20 15:56:54 +0530

    Updated MyMain.java with some basic starter code

--- src/MyMain.java
+++ src/MyMain.java
@@ -1,1 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class MyMain {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Please Enter Your Choice :: ");
+        System.out.println("1. Server");
+        System.out.println("2. Client");
+    }
+}
 
