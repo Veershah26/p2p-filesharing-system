95c1b75596ef7588e7056262e77781ceaef84420
Veershah26 <shah.veer18@gmail.com>
2021-11-20 16:00:57 +0530

    Updated MyMain.java to read Input Stream to give user choice between server and client

--- src/MyMain.java
+++ src/MyMain.java
@@ -6,6 +6,21 @@ public class MyMain {
         System.out.println("Please Enter Your Choice :: ");
         System.out.println("1. Server");
         System.out.println("2. Client");
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int choice = Integer.parseInt(br.readLine());
+
+        if(choice == 1) {
+            Server s = new Server();
+        }
+
+        else if(choice == 2) {
+            Client c = new Client();
+        }
+
+        else {
+            System.out.println("Your Choice Is Incorrect");
+        }
     }
 }
 
